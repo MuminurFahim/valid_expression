359598efb54a7d5133513d488993db256afbd3fd
Muminur Fahim <121749007+MuminurFahim@users.noreply.github.com>
2024-02-11 13:58:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('form').addEventListener('submit', (e) => {
     const index = document.getElementById('select').selectedIndex;
     if (index == 0) {
         p.setAttribute('style', 'color:blue');
-        p.innerHTML = 'Please select an option before entering an input.';
+        p.innerHTML = '* Please select an option before entering an input.';
     } else {
         const regexs = [
                 /^([0-9a-zA-Z]([-\.\+\_\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/,
@@ -17,7 +17,7 @@ document.getElementById('form').addEventListener('submit', (e) => {
             options = ['Email', 'Postcode', 'Mobile Number'],
             text = document.getElementById('text').value;
         if (regexs[index - 1].test(text)) {
-            p.setAttribute('style', 'color:green');
+            p.setAttribute('style', 'color:limegreen');
             p.innerHTML = `'${text}' is a <b>valid</b> ${options[index - 1]}.`;
         } else {
             p.setAttribute('style', 'color:red');
